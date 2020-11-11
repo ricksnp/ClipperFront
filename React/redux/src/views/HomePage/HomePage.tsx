2de1f9a7a4284d9cbd4d58decb931c11a13e32bf
Dawit Wondim <dawit346@gmail.com>
2020-11-10 22:04:20 -0800

    Sass Testing

--- React/redux/src/views/HomePage/HomePage.tsx
+++ React/redux/src/views/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../index.css';
+import '../../index.scss';
 import ProfilePane from "../../components/ProfilePane/ProfilePane"
 import {Feed} from './../../components/Feed/Feed'
 
