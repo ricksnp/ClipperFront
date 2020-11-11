59ec5b6b50e5515e0ceb849e42fd7f2324ed4afb
Matthew Thomas <matthew.thomas@revature.net>
2020-11-11 12:42:01 -0600

    Added Search Users

--- React/redux/src/components/Feed/Feed.tsx
+++ React/redux/src/components/Feed/Feed.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-
-import './../../views/LandingPage/LandingPage.css';
-import dark_theme_logo from './../../Assets/Clipper-Logo-Dark-Theme.png';
 import Posts from '../Posts/Posts';
-import {NewPost} from './../NewPost/NewPost';
+import TextEditor from '../TextEditor/TextEditor';
+import dark_theme_logo from './../../Assets/Clipper-Logo-Dark-Theme.png';
+import './../../views/LandingPage/LandingPage.css';
+import { NewPost } from './../NewPost/NewPost';
+
 
 export function Feed(props:any){
     return(
         <div className = "col-md-9" style={{padding:'0px'}}>
-            <div className = "BlueBackground">
+            <div className = "BlueBackground row" style={{display:"flex", alignItems:"center"}}>
+                <div className="col-4"></div>
                 <img src={dark_theme_logo} className = "logo-small mx-auto" style={{width: "50px", height: "50px", padding:'10px', display:"block"}}></img>
+                <span>
+                    <span><label>Search Users: </label><input type="text" placeholder="Find User"></input> <input type="submit" value="Search"/></span>
+                </span>
                 <p className='d-md-none d-l-none d-xl-none' style={{textAlign: "center"}}>Welcome to <strong>Clipper!</strong><br/>Please <a href="#">Login</a> or <a href="#">Create an account</a> to post.</p>
             </div>
             <div className = "GreyBackground " style={{padding:'0px'}}>
