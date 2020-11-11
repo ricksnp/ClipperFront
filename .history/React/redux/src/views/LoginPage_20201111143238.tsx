76d882dd01b6e1e860d36c4301e36eaa6f4b4427
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2020-11-11 14:48:02 -0500

    Working

--- .history/React/redux/src/views/LoginPage_20201111143238.tsx
+++ .history/React/redux/src/views/LoginPage_20201111143238.tsx
@@ -8,14 +8,14 @@ export default function LoginPage() {
 
         <div className="d-md-flex h-md-100 ">
             <div className="col-md-8 p-0 bg-indigo h-md-100">
-                <div className="text-white d-md-flex align-items-center h-md-100 p-5 text-center justify-content-center" id='div1'>
+                <div className="text-white d-md-flex align-items-center h-md-100 p-5 text-center justify-content-center" id = 'div1'>
                     <LoginComponent />
                 </div>
             </div>
 
-            <div className="col-md-4 p-0 bg-white h-md-100 loginarea" id="sideBar">
-                <div className="d-md-flex align-items-center h-md-100 p-5 justify-content-center" id='div2'>
-                    <div id='div3'>Hello World</div>
+            <div className="col-md-4 p-0 bg-white h-md-100 loginarea">
+                <div className="d-md-flex align-items-center h-md-100 p-5 justify-content-center" id = 'div2'>
+                    <div id = 'div3'>Hello World</div>
                 </div>
             </div>
         </div>
